af5c6b7ab6bbfbe8f37a846f1acaaac5d9f9141b
zeekkeen <zekken2307@gmail.com>
2019-10-01 13:57:05 -0400

    editando el enemigo 1

--- Assets/02_Scripts/Enemies/Enemy.cs
+++ Assets/02_Scripts/Enemies/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (BoxCollider2D))]
+[RequireComponent (typeof (Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
     public int health;
@@ -13,54 +14,25 @@ public class Enemy : MonoBehaviour
     public GameObject bloodSplash,corpse;
     Animator anim;
     //RipplePostProcessor camRipple;
-    ////
-
-    public float maxJumpHeight = 4;
-	public float minJumpHeight = 1;
-	public float timeToJumpApex = .4f;
-	float accelerationTimeAirborne = .2f;
-	float accelerationTimeGrounded = .1f;
-	public float moveSpeed = 6;
-
-	public Vector2 wallJumpClimb;
-	public Vector2 wallJumpOff;
-	public Vector2 wallLeap;
-
-	public float wallSlideSpeedMax = 3;
-	public float wallStickTime = .25f;
-	float timeToWallUnstick;
-
-	float gravity;
-	float maxJumpVelocity;
-	float minJumpVelocity;
-	Vector3 velocity;
-	float velocityXSmoothing;
-
-
-	Vector2 directionalInput;
-	bool wallSliding;
-	int wallDirX;
 	bool facingRight=true;
 	float dashTime=0f;
 	public float startDashTime=0.1f,dashSpeed=7f,distance;
 	bool dash=false;
 	public GameObject dashEffect,groundDetection;
+
+	Rigidbody2D rb;
     void Start()
     {
         //anim=GetComponent<Animator>();
 		anim=GetComponentInChildren<Animator>();
         anim.SetBool("isRunning",true);
+		rb=GetComponent<Rigidbody2D>();
         //camRipple=Camera.main.GetComponent<RipplePostProcessor>();
 
-        //controller = GetComponent<Controller2D> ();
-		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
-		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
-        directionalInput=new Vector2(1,0);
     }
 
 	void Update() {
-		
+		rb.velocity = ((facingRight)?Vector2.right*speed:Vector2.left*speed);
 	}
 
 	void flip(){
@@ -68,7 +40,6 @@ public class Enemy : MonoBehaviour
 		Vector3 scaler=transform.localScale;
 		scaler.x*=-1;
 		transform.localScale=scaler;
-        directionalInput*=-1;
 	}
     public void TakeDamage(int damage)
     {
