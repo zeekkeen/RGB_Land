57f1d5392805714b56afa63dddde81d8675b8625
zeekkeen <zekken2307@gmail.com>
2019-10-01 13:34:18 -0400

    se agrego el ataque de rango y animacion

---
+++ Assets/02_Scripts/Projectile/Projectile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public int damage=10;
+    public GameObject destroyEffect;
+	Rigidbody2D rb;
+
+	Vector2 directionalInput;
+	bool facingRight=true;
+	float moveTime=0f;
+	public float startTime=1f,speed=10f;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D> ();
+        moveTime=startTime;
+	}
+
+	void Update() {
+        if(transform.localScale.x>0)
+			rb.velocity = new Vector2(1 * speed * Time.deltaTime, rb.velocity.y);
+        else 
+			rb.velocity = new Vector2(-1 * speed * Time.deltaTime, rb.velocity.y);
+			
+		moveTime-=Time.deltaTime;
+		if(moveTime<=0)
+		{
+			moveTime=0;
+            DestroyProjectil();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+			if (other.gameObject.CompareTag("Enemy"))
+            {
+                other.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            DestroyProjectil();
+	}
+
+    void DestroyProjectil()
+    {
+        Instantiate(destroyEffect,transform.position,Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
