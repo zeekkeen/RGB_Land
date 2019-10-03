c41c11e5e4f5e05d69181821b882ba91b16c8bb9
zeekkeen <zekken2307@gmail.com>
2019-10-03 14:57:03 -0400

    agregando varias cosas

--- Assets/02_Scripts/Enemies/Enemy.cs
+++ Assets/02_Scripts/Enemies/Enemy.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 [RequireComponent (typeof (Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
-    public int health;
+    public SpriteRenderer lifeRender;
+    public int health = 0,maxHealth=0;
     public float speed;
     float dazedTime;
     public float StartDazedTime = 0.6f;
     public GameObject bloodEffect,deathEffect;
     public GameObject bloodSplash,corpse;
     Animator anim;
-    //RipplePostProcessor camRipple;
+    RipplePostProcessor camRipple;
 	bool facingRight=true;
 	float dashTime=0f;
 	public float startDashTime=0.1f,dashSpeed=7f,distance;
@@ -21,17 +22,26 @@ public class Enemy : MonoBehaviour
 	public GameObject dashEffect,groundDetection;
 
 	Rigidbody2D rb;
-    void Start()
-    {
+    void Start(){
+        maxHealth=health;
         //anim=GetComponent<Animator>();
 		anim=GetComponentInChildren<Animator>();
-        anim.SetBool("isRunning",true);
+        //anim.SetBool("isRunning",true);
 		rb=GetComponent<Rigidbody2D>();
-        //camRipple=Camera.main.GetComponent<RipplePostProcessor>();
+        camRipple=Camera.main.GetComponent<RipplePostProcessor>();
 
     }
 
 	void Update() {
+        if(dazedTime<=0)
+            speed=5;
+        else{
+            speed=0;
+            dazedTime-=Time.deltaTime;
+        }
+        RaycastHit2D groundInfo=Physics2D.Raycast(groundDetection.transform.position,Vector2.down,distance);
+        if (!groundInfo.collider)
+            flip();
 		rb.velocity = ((facingRight)?Vector2.right*speed:Vector2.left*speed);
 	}
 
@@ -41,21 +51,28 @@ public class Enemy : MonoBehaviour
 		scaler.x*=-1;
 		transform.localScale=scaler;
 	}
-    public void TakeDamage(int damage)
-    {
+
+    public void TakeDamage(int damage){
         Instantiate(bloodEffect,transform.position,Quaternion.identity);
         dazedTime=StartDazedTime;
         health-=damage;
+        ChangeColor1();
         Debug.Log("damague taken!!");
 		if(health <= 0)
 			Dead();
     }
-    public void Dead()
-    {
-        //camRipple.RippleEffect();
+    public void Dead(){
+        camRipple.RippleEffect();
         Instantiate(corpse,transform.position,Quaternion.identity);
         Instantiate(bloodSplash,transform.position,Quaternion.identity);
         Instantiate(deathEffect,transform.position,Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    public void ChangeColor1(){
+        float alpha,percentOfHealth;
+        percentOfHealth = (health*100) / maxHealth;
+        alpha = percentOfHealth / 100;
+        lifeRender.color = new Color(lifeRender.color.r, lifeRender.color.g, lifeRender.color.b, alpha);
     }
 }
