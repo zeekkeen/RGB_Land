984fad4a0a28f896ce4b1b5606bcdf4c85c902e9
zeekkeen <zekken2307@gmail.com>
2019-11-14 15:50:56 -0400

    fixed button C and X

--- Assets/02_Scripts/General/ButtonThroughKeySelection.cs
+++ Assets/02_Scripts/General/ButtonThroughKeySelection.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ButtonThroughKeySelection : MonoBehaviour{
-    // public void Update(){
-    //     if (Input.anyKeyDown)
-    //         EventSystem.current.SetSelectedGameObject(this.gameObject);
-    // }
+
+    public bool active = true;
+
+    public void Update(){
+        if (Input.anyKeyDown && !active){
+            ChangeFocus();
+            Debug.Log("any key");
+        }
+        if (Input.GetMouseButton(0) && active){
+            active = false;
+            Debug.Log("mouse");
+        }
+    }
 
     public void ChangeFocus(){
         EventSystem.current.SetSelectedGameObject(this.gameObject);
+        active = true;
     }
 }
