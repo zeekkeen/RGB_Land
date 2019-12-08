02ae0061c6e977354414fc8364d8d87b64798ad9
zeekkeen <zekken2307@gmail.com>
2019-12-07 22:07:22 -0400

    mejorado

--- Assets/02_Scripts/Color objects/NormalForce.cs
+++ Assets/02_Scripts/Color objects/NormalForce.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class NormalForce : MonoBehaviour{
 
     public float force;
-    
+
     void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.tag == "Player" ){
 			Debug.Log("entro");
 			Vector3 vec = other.transform.position - transform.position;
 			other.gameObject.GetComponent<Rigidbody2D>().AddForce(vec.normalized * force);
-			// other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * force);
+			other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * force);
+            ITakeDamage takeDamage = other.GetComponent<ITakeDamage>();
+            if(takeDamage != null)
+                takeDamage.TakeDamage(1);
 		}
 	}
 }
