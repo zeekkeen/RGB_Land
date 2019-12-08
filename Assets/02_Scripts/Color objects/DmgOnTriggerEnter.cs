f973a20c4417d6200a0a236cd65063264c9ace70
zeekkeen <zekken2307@gmail.com>
2019-12-07 20:44:56 -0400

    puas

---
+++ Assets/02_Scripts/Color objects/DmgOnTriggerEnter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DmgOnTriggerEnter : MonoBehaviour{
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        ITakeDamage takeDamage = other.GetComponent<ITakeDamage>();
+        if(takeDamage != null)
+            takeDamage.TakeDamage(1);
+    }
+}
