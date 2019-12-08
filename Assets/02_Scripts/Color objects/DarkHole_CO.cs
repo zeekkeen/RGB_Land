bba33f573459f8237dbfb0cdd8f559af9b824cce
zeekkeen <zekken2307@gmail.com>
2019-12-07 22:05:38 -0400

    darkHole

---
+++ Assets/02_Scripts/Color objects/DarkHole_CO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DarkHole_CO : ColorObject {
+
+	public float force;
+
+	public override void PaintedAction(){
+        base.PaintedAction();
+    }
+
+    public override void UnPaintedAction(){
+        base.UnPaintedAction();
+    }
+
+	private void OnTriggerStay2D(Collider2D other) {
+		if(other.gameObject.tag == "Player" && !painted){
+			
+			Vector3 vec = other.transform.position - transform.position;
+			float distance = Vector3.Distance(other.transform.position, transform.position);
+			other.gameObject.GetComponent<Rigidbody2D>().AddForce(vec.normalized * (force / distance) * (-1));
+			// other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * force);
+		}
+	}
+}
---
+++ Assets/02_Scripts/Color objects/NormalForce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalForce : MonoBehaviour{
+
+    public float force;
+    
+    void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player" ){
+			Debug.Log("entro");
+			Vector3 vec = other.transform.position - transform.position;
+			other.gameObject.GetComponent<Rigidbody2D>().AddForce(vec.normalized * force);
+			// other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * force);
+		}
+	}
+}
